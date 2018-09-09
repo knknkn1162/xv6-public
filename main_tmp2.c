6a947cb31a141db28d0017bb63caf2e49d523d66
knknkn <knknkn1162@gmail.com>
2018-09-09 22:58:31 +0900

    add comment in the protected mode

---
+++ main_tmp2.c
@@ -0,0 +1,12 @@
+// Bootstrap processor starts running C code here.
+// Allocate a real stack and switch to it, first
+// doing some setup required for memory allocator to work.
+//
+unsigned int entrypgdir[1024];  // For entry.S
+
+int
+main(void)
+{
+  for(;;)
+    ;
+}
