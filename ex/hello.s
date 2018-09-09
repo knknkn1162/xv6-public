22f5dba8ef2c4c65cd96c30b3e01f1edda01d855
knknkn <knknkn1162@gmail.com>
2018-09-09 16:27:40 +0900

    hello.s

---
+++ ex/hello.s
@@ -0,0 +1,18 @@
+.text
+  .globl main
+
+main:
+  movl $0x04, %eax
+  movl $0x01, %ebx
+  movl $message, %ecx
+  movl $12, %edx # length
+  int $0x80
+  call exit
+exit:
+  movl $0x01, %eax
+  xorl %ebx, %ebx
+  int $0x80
+  
+.section .rodata
+message:
+  .ascii "Hello world\n"
