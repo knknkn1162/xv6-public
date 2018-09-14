5b38a8eb9ce7433dbf1154c3a6f0addd26c4d9ad
knknkn <knknkn1162@gmail.com>
2018-09-14 16:43:23 +0900

    add comment on boot process

--- asm.h
+++ asm.h
@@ -12,7 +12,8 @@
         .word (((lim) >> 12) & 0xffff), ((base) & 0xffff);      \
         .byte (((base) >> 16) & 0xff), (0x90 | (type)),         \
                 (0xC0 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
-
+// non-comforming
+// Transfer into a nonconforming segment at a different priviledge level results in a general-protection exception, unless a callgate or task gate is used.
 #define STA_X     0x8       // Executable segment
 #define STA_W     0x2       // Writeable (non-executable segments)
 #define STA_R     0x2       // Readable (executable segments)
